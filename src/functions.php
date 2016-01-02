8b615afc3c8ed6c70426a6c2ee2759ca3b3b4c72
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-01-02 02:18:39 +0100

    Remove file headers

--- src/functions.php
+++ src/functions.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Supervisor Configuration package.
- *
- * (c) Márk Sági-Kazár <mark.sagikazar@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 /**
  * These functions are in the global namespace, because Symfony Options Resolver only supports is_* functions in the global namespace.
  */
